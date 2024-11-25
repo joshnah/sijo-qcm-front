22c6e4ceef7f702c63d5f9297d1f65ab8a11d73f
Antoine Klein <antoineklein2000@gmail.com>
2024-11-25 11:55:42 +0100

    linked quiz from backend

--- src/app/features/quiz/components/quiz-taker/quiz-taker.component.ts
+++ src/app/features/quiz/components/quiz-taker/quiz-taker.component.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
-import { SampleQuiz } from '../../mocks/quiz.mock';
+import { Component, OnInit, computed, inject } from '@angular/core';
 import { QuestionsContainerComponent } from '../questions-container/questions-container.component';
+import { QuizService } from '../../services/quiz.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-quiz-taker',
   standalone: true,
-  imports: [QuestionsContainerComponent],
+  imports: [CommonModule, QuestionsContainerComponent],
   templateUrl: './quiz-taker.component.html',
-  styleUrl: './quiz-taker.component.css',
+  styleUrls: ['./quiz-taker.component.css'],
 })
-export class QuizTakerComponent {
-  haveStarted = false
-  quiz = SampleQuiz;
+export class QuizTakerComponent implements OnInit {
+  haveStarted = false;
+  // Reactively read quizzes from the signal
+  quizzes = computed(() => this.quizService.quizSignal());
+  selectedQuiz = computed(() => (this.quizzes() ? this.quizzes() : null)); // Default to the first quiz if available
+
+  constructor(private quizService: QuizService) {}
+
+  ngOnInit(): void {
+    this.quizService.fetchAndSetQuizzes().subscribe({
+      error: (err) => console.error('Error loading quizzes:', err),
+    });
+  }
+
+  startQuiz(): void {
+    this.haveStarted = true;
+  }
 }
--- src/app/features/quiz/services/quiz.service.ts
+++ src/app/features/quiz/services/quiz.service.ts
@@ -1,8 +1,10 @@
 import { inject, Injectable, signal } from '@angular/core';
 import { Quiz, SelectedResponses } from '../../../shared/models/quiz.model';
-import { SampleQuiz } from '../mocks/quiz.mock';
+// import { SampleQuiz } from '../mocks/quiz.mock';
 import { Observable, of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { SampleQuiz } from '../mocks/quiz.mock';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +12,7 @@ import { HttpClient } from '@angular/common/http';
 export class QuizService {
   http = inject(HttpClient)
   constructor() { }
-  quizSignal = signal<Quiz[]>([])
+  quizSignal = signal<Quiz| null>(null)
   quiz$ = this.quizSignal.asReadonly();
 
   // TODO: Config quiz backend service
@@ -19,8 +21,12 @@ export class QuizService {
   }
 
 
-  fetchQuiz():void{
-   this.quizSignal.set([SampleQuiz]);
+  fetchAndSetQuizzes(): Observable<Quiz> {
+    return this.http.get<Quiz>("http://localhost:8080/qcm/1").pipe(
+      tap((quizzes) => {
+        this.quizSignal.set(quizzes);
+      })
+    );
   }
 
 
