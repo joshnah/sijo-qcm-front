e07972cf0686dd29e8c9727c439e417b68160187
joshnah <tiendatamser296@gmail.com>
2024-12-07 12:31:34 +0100

    fix: fix environment.ts

--- src/app/core/interceptors/api.interceptor.ts
+++ src/app/core/interceptors/api.interceptor.ts
@@ -1,5 +1,5 @@
 import { HttpInterceptorFn } from '@angular/common/http';
-import { environment } from '../../../environments/environment.development';
+import { environment } from '../../../environments/environment';
 
 export const apiInterceptor: HttpInterceptorFn = (req, next) => {
   const apiReq = req.clone({ url: `${environment.backendUrl}${req.url}` });
