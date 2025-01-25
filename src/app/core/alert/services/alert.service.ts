b962b738abea1e589e422ab9e797c24fd3bda8cb
joshnah <tiendatamser296@gmail.com>
2025-01-25 23:21:47 +0100

    feat: auto close alert message

--- src/app/core/alert/services/alert.service.ts
+++ src/app/core/alert/services/alert.service.ts
@@ -2,18 +2,20 @@ import { Injectable, signal } from '@angular/core';
 import { Alert } from '../models/alert.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AlertService {
   alertMessage = signal<Alert | null>(null);
-  constructor() { 
-  }
+  constructor() {}
 
-  setMessage(alert: Alert): void{
+  setMessage(alert: Alert): void {
     this.alertMessage.set(alert);
+    setTimeout(() => {
+      this.deleteMessage();
+    }, 5000);
   }
 
-  deleteMessage():void{
+  deleteMessage(): void {
     this.alertMessage.set(null);
   }
 }
